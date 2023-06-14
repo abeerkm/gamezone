956ef7c57b4fe552c019e051cd55065671651c95
abeerkm <68709596+abeerkm@users.noreply.github.com>
2023-06-14 18:18:14 +0300

    reviews

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Home from './screens/home';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const getFonts = () => Font.loadAsync({
+  'BeVietnamPro-Black': require('./assets/fonts/BeVietnamPro-Black.ttf'),
+  'BeVietnamPro-BlackItalic': require('./assets/fonts/BeVietnamPro-BlackItalic.ttf'),
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts} 
+        onFinish={() => setFontsLoaded(true)} 
+      />
+    )
+  }
+
+}
---
+++ screens/about.js
@@ -0,0 +1,15 @@
+import react from "react";
+import { StyleSheet,View,Text  } from "react-native";
+
+export default function About(){
+    return(
+        <view style={styles.container}>
+            <Text></Text>
+        </view>
+    )
+}
+const styles= StyleSheet.create({
+    container:{
+        padding:24
+    }
+});
